eea907ee3b514cb4e80a49ac9270bcb143fad314
dissolete <aequus.junk@yahoo.com>
2016-08-20 19:02:48 -0700

    Added GroupManager class. Not entirely flushed out.  Error handling needs to be implemented.

---
+++ CRE/src/CRGroupManager.cpp
@@ -0,0 +1,48 @@
+#include "CRGroupManager.hpp"
+
+namespace CRE
+{
+    GroupManager::GroupManager()
+    {}
+
+    GroupManager::~GroupManager()
+    {
+        // Cleanup all entities
+        for(auto it = _groups.begin(); it != _groups.end(); ++it)
+        {
+            it->second->cleanup();
+        }
+    }
+
+    void GroupManager::add(Group * newGroup)
+    {
+        // TODO: Check if the group already exists in the map, if it does,
+        // log that it does and quit app?
+
+        // Check if the ptr is not bad
+        if( newGroup != NULL )
+            _groups.insert(std::pair<std::string, Group*>(newGroup->get_ID(), newGroup));
+    }
+
+    void GroupManager::create_new(std::string groupName)
+    {
+        // TODO: Check if the group already exists in the map, if it does,
+        // log that it does and quit app?
+
+        // Allocate new group
+        Group * newGroup = new(std::nothrow) Group(groupName);
+
+        if( newGroup != NULL )
+            _groups.insert(std::pair<std::string, Group*>(newGroup->get_ID(), newGroup));
+
+        // Remove local pointer
+        newGroup = NULL;
+
+    }
+
+    Group& GroupManager::get(std::string groupName)
+    {
+        // TODO: What to do if the group doesn't exist?
+        return *(_groups.at(groupName));
+    }
+}
